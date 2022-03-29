6fdc2e78c1ca37b095c8a9dc9162a367014d930b
Eliseo Murillo <eliseomurillo9@gmail.com>
2022-03-29 11:05:49 +0200

    image fail

--- src/views/webdev.vue
+++ src/views/webdev.vue
@@ -1,35 +1,63 @@
  <template>
   <v-parallax
     scale="1.4"
-    height="100vh"
+    height="90vh"
     class="responsive-portrait"
     src="/assets/img/union.jpg"
     ><div class="logo"><img src="/assets/img/logoem.png" alt="logo" /></div>
   </v-parallax>
   <v-parallax scale="1.3" dark src="/assets/img/backgroundrombos.jpg">
+  <h2 class="about-title">Web Projects</h2>
     <v-row align="center" justify="center">
-      <v-col class="text-center box mt-12 ml-sm-5 mr-sm-5" cols="10">
-        <v-img style="cursor:pointer" @click="goToWebsite('https://eliseomurillo9.github.io')"  :src="imageSite" />
+      <v-col class="text-center image-a box mt-xs-n7 mt-sm-n7 ml-sm-5 mr-sm-5 " cols="10">
+        
+        <v-img
+          style="cursor: pointer"
+          @click="goToWebsite('https://eliseomurillo9.github.io')"
+          :src="imageSite"
+        />
       </v-col>
     </v-row>
     <v-row align="center" justify="center">
-      <v-col class="text-center box mt-12 ml-sm-5 mr-sm-5" cols="10">
-        <h2>Projects done for FreeCodeCamp certification</h2>
-          <iframe class="centerPen" scrolling="no" title="IKEA" src="https://codepen.io/eliseomurillo9/embed/XWaKYmP?default-tab=result&theme-id=dark" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-  See the Pen <a href="https://codepen.io/eliseomurillo9/pen/XWaKYmP">
-  IKEA</a> by Josue (<a href="https://codepen.io/eliseomurillo9">@eliseomurillo9</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-</iframe>
+      <v-col class="text-center box  ml-sm-5 mr-sm-5" cols="10">
+        <h2 class="about-title">
+          Projects done for FreeCodeCamp certification
+        </h2>
+        <iframe
+          class="centerPen"
+          scrolling="no"
+          title="IKEA"
+          src="https://codepen.io/eliseomurillo9/embed/XWaKYmP?default-tab=result&theme-id=dark"
+          frameborder="no"
+          loading="lazy"
+          allowtransparency="true"
+          allowfullscreen="true"
+        >
+          See the Pen
+          <a href="https://codepen.io/eliseomurillo9/pen/XWaKYmP"> IKEA</a> by
+          Josue (<a href="https://codepen.io/eliseomurillo9">@eliseomurillo9</a
+          >) on <a href="https://codepen.io">CodePen</a>.
+        </iframe>
       </v-col>
-    </v-row >
+    </v-row>
     <v-row align="center" justify="center">
-    <v-col class="text-center box  ml-sm-5 mr-sm-5 mb-10" cols="10">
-      <iframe class="centerPen" scrolling="no" title="JS" src="https://codepen.io/eliseomurillo9/embed/QWMdEwq?default-tab=result&theme-id=dark" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-  See the Pen <a href="https://codepen.io/eliseomurillo9/pen/QWMdEwq">
-  JS</a> by Josue (<a href="https://codepen.io/eliseomurillo9">@eliseomurillo9</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-</iframe>
-    </v-col>
+      <v-col class="text-center box mt-sm-n7 ml-sm-5 mr-sm-5 mb-10" cols="10">
+        <iframe
+          class="centerPen"
+          scrolling="no"
+          title="JS"
+          src="https://codepen.io/eliseomurillo9/embed/QWMdEwq?default-tab=result&theme-id=dark"
+          frameborder="no"
+          loading="lazy"
+          allowtransparency="true"
+          allowfullscreen="true"
+        >
+          See the Pen
+          <a href="https://codepen.io/eliseomurillo9/pen/QWMdEwq"> JS</a> by
+          Josue (<a href="https://codepen.io/eliseomurillo9">@eliseomurillo9</a
+          >) on <a href="https://codepen.io">CodePen</a>.
+        </iframe>
+      </v-col>
     </v-row>
   </v-parallax>
 </template>
@@ -44,17 +72,22 @@
   margin-top: 7%;
 }
 
-.centerPen{
-  height:731px;
+.centerPen {
+  height: 731px;
   width: 100%;
 }
 
-h2 {
-  font-size: 50px;
+.image-a {
+  height: 400px;
+  transition-duration: 1s;
+  transition-delay: 1s;
+}
+
+.about-title {
+  font-size: 5rem;
   text-align: center;
-  color: #fff;
+  color: #3466aa;
   line-height: 1;
-  font-weight: 500;
   font-family: Montserrat;
   font-weight: 600;
   margin-bottom: 50px;
@@ -72,16 +105,18 @@ h2 {
     height: 50vh;
   }
 
-  .centerPen{
-  height:400px;
-  width: 100%;
+  .centerPen {
+    height: 400px;
+    width: 100%;
+  }
+.image-a {
+  height: 200px;
 }
-
-h2 {
+  .about-title {
     font-size: 20px;
-    text-align: end;
-    color: #fff;
-    line-height: 1;
+    text-align: center;
+    margin-bottom: 10px;
+  margin-top: 10px;
   }
 }
 </style>
@@ -93,10 +128,9 @@ export default {
     imageSite: "/assets/img/siteweb.jpg",
   }),
   methods: {
-      goToWebsite(site){
-        window.open(site,'_blank')
-      }
-  }
+    goToWebsite(site) {
+      window.open(site, "_blank");
+    },
+  },
 };
-
 </script>
