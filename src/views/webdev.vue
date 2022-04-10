2b56ece4bb4e9dd484a5812c093eca721cdf1b12
Eliseo Murillo <eliseomurillo9@gmail.com>
2022-04-10 10:52:39 +0200

    webdevelopment

--- src/views/webdev.vue
+++ src/views/webdev.vue
@@ -7,58 +7,89 @@
     ><div class="logo"><img src="/assets/img/logoem.png" alt="logo" /></div>
   </v-parallax>
   <v-parallax scale="1.3" dark src="/assets/img/backgroundrombos.jpg">
-  <h2 class="about-title">Web Projects</h2>
-    <v-row align="center" justify="center">
-      <v-col class="text-center image-a box mt-xs-n7 mt-sm-n7 ml-sm-5 mr-sm-5 " cols="10">
-        
-        <v-img
-          style="cursor: pointer"
-          @click="goToWebsite('https://eliseomurillo9.github.io')"
-          :src="imageSite"
-        />
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col class="text-center box  ml-sm-5 mr-sm-5" cols="10">
-        <h2 class="about-title">
-          Projects done for FreeCodeCamp certification
-        </h2>
-        <iframe
-          class="centerPen"
-          scrolling="no"
-          title="IKEA"
-          src="https://codepen.io/eliseomurillo9/embed/XWaKYmP?default-tab=result&theme-id=dark"
-          frameborder="no"
-          loading="lazy"
-          allowtransparency="true"
-          allowfullscreen="true"
-        >
-          See the Pen
-          <a href="https://codepen.io/eliseomurillo9/pen/XWaKYmP"> IKEA</a> by
-          Josue (<a href="https://codepen.io/eliseomurillo9">@eliseomurillo9</a
-          >) on <a href="https://codepen.io">CodePen</a>.
-        </iframe>
-      </v-col>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-col class="text-center box mt-sm-n7 ml-sm-5 mr-sm-5 mb-10" cols="10">
-        <iframe
-          class="centerPen"
-          scrolling="no"
-          title="JS"
-          src="https://codepen.io/eliseomurillo9/embed/QWMdEwq?default-tab=result&theme-id=dark"
-          frameborder="no"
-          loading="lazy"
-          allowtransparency="true"
-          allowfullscreen="true"
-        >
-          See the Pen
-          <a href="https://codepen.io/eliseomurillo9/pen/QWMdEwq"> JS</a> by
-          Josue (<a href="https://codepen.io/eliseomurillo9">@eliseomurillo9</a
-          >) on <a href="https://codepen.io">CodePen</a>.
-        </iframe>
-      </v-col>
-    </v-row>
+    <div>
+      <div class="title">
+        <h2>Projects</h2>
+      </div>
+      <div class="cards">
+        <div>
+        <v-card class="card">
+          <v-img src="/assets/img/enterprisephone.jpg" height="200px" cover></v-img>
+          <v-card-title style="color: #3363a4">
+            Company
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn
+              color="black-lighten-2"
+              variant="text"
+              @click="goToWebsite('https://eliseomurillo9.github.io')"
+            >
+              See site
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show = !show"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+              <v-card-title style="color: #3363a4">Languages</v-card-title>
+              <v-card class="logos">
+                <img src="/assets/img/732212.png" />
+                <img src="/assets/img/Devicon-css3-plain.png" />
+                <img src="/assets/img/600px-JavaScript-logo.png" />
+              </v-card>
+            </div>
+          </v-expand-transition>
+        </v-card>
+        </div>
+        <div>
+        <v-card class="card">
+          <v-img src="/assets/img/homeandgardenphone.jpg" height="200px" cover></v-img>
+
+          <v-card-title style="color: #3363a4">
+            Home & Garden
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn
+              color="black-lighten-2"
+              variant="text"
+              @click="goToWebsite('https://homegarden-pgaxg.ondigitalocean.app')"
+            >
+              See site
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              :icon="show1 ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show1 = !show1"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show1">
+              <v-divider></v-divider>
+              <v-card-title style="color: #3363a4">Languages</v-card-title>
+              <v-card class="logos">
+                <img src="/assets/img/732212.png" />
+                <img src="/assets/img/Devicon-css3-plain.png" />
+                <img src="/assets/img/600px-JavaScript-logo.png" />
+              </v-card>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </div>
+    </div>
+    
+    </div>
   </v-parallax>
 </template>
 <style scoped>
@@ -72,27 +103,54 @@
   margin-top: 7%;
 }
 
-.centerPen {
-  height: 731px;
-  width: 100%;
+.title{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 
-.image-a {
-  height: 400px;
-  transition-duration: 1s;
-  transition-delay: 1s;
-}
-
-.about-title {
-  font-size: 5rem;
+.title h2{
+  font-size: 35px;
+  color: aquamarine;
   text-align: center;
-  color: #3466aa;
-  line-height: 1;
+  margin: 25px 0 25px 0;
   font-family: Montserrat;
-  font-weight: 600;
-  margin-bottom: 50px;
-  margin-top: 100px;
 }
+
+.cards {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+
+.logos {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.logos img {
+  height: 50px;
+  margin: 15px;
+  filter: drop-shadow(2px 3px 2px #4b4b4d);
+
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+}
+
+.logos img:hover {
+  transform: scale(1.3);
+  filter: drop-shadow(3px 4px 3px #4b4b4d, 20%);
+}
+
+.card {
+  width: 404px;
+  padding: 0%;
+  margin: 0%;
+}
+
+
+
 @media (max-width: 900px) {
   .logo {
     text-align: center;
@@ -109,15 +167,28 @@
     height: 400px;
     width: 100%;
   }
-.image-a {
-  height: 200px;
-}
+  .image-a {
+    height: 200px;
+  }
   .about-title {
     font-size: 20px;
     text-align: center;
     margin-bottom: 10px;
-  margin-top: 10px;
+    margin-top: 10px;
   }
+
+  .cards{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;  
+    align-items: center;
+  }
+
+  .card {
+  width: 204px;
+  padding: 0%;
+  margin-top: 15px;
+}
 }
 </style>
 
@@ -126,6 +197,8 @@ export default {
   data: () => ({
     mirror: true,
     imageSite: "/assets/img/siteweb.jpg",
+    show: false,
+    show1: false,
   }),
   methods: {
     goToWebsite(site) {
